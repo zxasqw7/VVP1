a0c91817de35c77bcd928182289f2161cd9f03f9
zxasqw7 <90841897+zxasqw7@users.noreply.github.com>
2021-09-30 15:44:01 +0300

    Update lab4.cpp

--- lab4/lab4.cpp
+++ lab4/lab4.cpp
@@ -1,13 +1,14 @@
-﻿#include <iostream>
+#include <iostream>
 using namespace std;
 
 int main()
 {
+	setlocale(LC_CTYPE, "Russian");
 	int a;
 	int b;
-	cout << "a: ";
+	cout << "Введите a: ";
 	cin >> a;
-	cout << "b: ";
+	cout << "Введите b: ";
 	cin >> b;
 	int S = a * b;
 	cout << "S = " << S << endl;
