13c1bf8a734f66f2097c7b4d61da0cbd806c5f8c
zxasqw7 <sviridov-127@yandex.ru>
2021-09-30 21:46:09 +0300

    VVP1
    VVP1

---
+++ lab4(4)/lab4(4).cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_CTYPE, "Russian");
+	float a;
+	cout << "Введите a: ";
+	cin >> a;
+	float b;
+	cout << "Введите b: ";
+	cin >> b;
+	float summ;
+	summ = abs(a) + abs(b);
+	cout << "Сумма модулей = " << summ << endl;
+	float diff;
+	diff = abs(a) - abs(b);
+	cout << "Разность модулей = " << diff << endl;
+	float mult;
+	mult = abs(a) * abs(b); 
+	cout<< "Произведение модулей = " << mult << endl;
+	float div;
+	div = abs(a) / abs(b);
+	cout << "Частное модулей = " << div;
+	return 0;
+}
