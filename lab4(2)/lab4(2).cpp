4fe13ed654d659d48f5040d41920067218794a12
zxasqw7 <90841897+zxasqw7@users.noreply.github.com>
2021-09-30 15:46:10 +0300

    Update lab4(2).cpp

--- lab4(2)/lab4(2).cpp
+++ lab4(2)/lab4(2).cpp
@@ -1,17 +1,17 @@
-﻿#include <iostream>
+#include <iostream>
 using namespace std;
 
 int main()
 {
+	setlocale(LC_CTYPE, "Russian");
 	float a;
-	cout << "a: ";
+	cout << "Введите a: ";
 	cin >> a;
 	float b;
-	cout << "b: ";
+	cout << "Введите b: ";
 	cin >> b;
 	float avg = (a + b) / 2;
 	cout << "average = " << avg;
 	return 0;
 }
-
  
