4fd2ab9fc3898c0d296e99923cfcb7b4b061b8f2
zxasqw7 <90841897+zxasqw7@users.noreply.github.com>
2021-09-30 15:47:55 +0300

    Update lab4(1).cpp

--- lab4(1)/lab4(1).cpp
+++ lab4(1)/lab4(1).cpp
@@ -3,12 +3,13 @@ using namespace std;
 
 int main()
 {
+    setlocale(LC_CTYPE, "Russian");
     float d;
-    cout << "d: ";
+    cout << "Введите d: ";
     cin >> d;
     float pi = 3.14;
     float L = pi * d;
-    cout << "L = " << L;
+    cout << "Длина окружности = " << L;
     return 0;
 }
 
