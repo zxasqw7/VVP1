50e4c6cdecee45164e529f59e4d288d9bc1c38a1
zxasqw7 <sviridov-127@yandex.ru>
2021-10-04 19:49:27 +0300

    s

---
+++ lab4(3)/lab4(3)/lab4(3).cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_CTYPE, "Russian");
+    float x;
+    cout << "Введите x: ";
+    cin >> x;
+    float y;
+    cout << "Введите y: ";
+    cin >> y;
+    float summ = x * x + y * y;
+    cout << "Сумма квадратов = " << summ << endl;
+    float diff = x * x - y * y;
+    cout << "Разность квадратов = " << diff << endl;
+    float mult = (x * x) * (y * y);
+    cout << "Произведение квадратов = " << mult << endl;
+    float div = (x * x) / (y * y);
+    cout << "Частное квадратов = " << div;
+    return 0;
+}
