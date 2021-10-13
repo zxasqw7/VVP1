b21911c1e42678298d5f0b597940e68ea0e3c6d4
zxasqw7 <sviridov-127@yandex.ru>
2021-10-13 22:53:23 +0300

    lab5(4)

---
+++ lab5(4)/lab5(4).cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Ru");
+    float x1, x2, x3, y1, y2, y3, dis1, dis2, dis3, P;
+    cout << "Введите координаты первой вершины треугольника: ";
+    cin >> x1 >> y1;
+    cout << "Введите координаты второй вершины треугольника: ";
+    cin >> x2 >> y2;
+    cout << "Введите координаты третьей вершины треугольника: ";
+    cin >> x3 >> y3;
+    dis1 = sqrt(pow((x2 - x1), 2) + pow((y2 - y1), 2));
+    dis2 = sqrt(pow((x3 - x2), 2) + pow((y3 - y2), 2));
+    dis3 = sqrt(pow((x1 - x3), 2) + pow((y1 - y3), 2));
+    P = dis1 + dis2 + dis3;
+    cout << "Периметр треугольника = " << P << endl;
+    return 0;
+}
+
