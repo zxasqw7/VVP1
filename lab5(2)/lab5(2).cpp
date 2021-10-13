356c0f202bcf1c3075dd3522e984aa4ea5d31fd9
zxasqw7 <sviridov-127@yandex.ru>
2021-10-13 21:04:31 +0300

    lab5(2)

---
+++ lab5(2)/lab5(2).cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Ru");
+    int A, C, B, d1, d2, mult;
+    cout << "Введите точку A: ";
+    cin >> A;
+    cout << "Введите точку C: ";
+    cin >> C;
+    cout << "Введите точку B: ";
+    cin >> B;
+    d1 = C - A;
+    d2 = B - C;
+    mult = d1 * d2;
+    cout << "Произведение длин отрезков = " << mult;
+    return 0;
+}
+
