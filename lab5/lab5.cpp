fde7c77a4757220e9d60bf38094eb10c49dad656
zxasqw7 <sviridov-127@yandex.ru>
2021-10-13 21:01:55 +0300

    lab5

---
+++ lab5/lab5.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+
+{
+    setlocale(LC_ALL, "rus");
+    float x1, x2, y1, y2, z, a;
+    cout << "Введите координаты первой точки: ";
+    cin >> x1 >> y1;
+    cout << "Введите координаты второй точки: ";
+    cin >> x2 >> y2;
+    a = pow((x2 - x1), 2) + pow((y2 - y1),2);
+    z = sqrt(a);
+    cout << "Расстояние между двумя точками равно: " << z;
+    
+}
+
