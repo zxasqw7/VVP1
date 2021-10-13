fc24d22c0cadb79f7365ad3a55e28ba1989585b4
zxasqw7 <sviridov-127@yandex.ru>
2021-10-13 21:05:06 +0300

    lab5(3)

---
+++ lab5(3)/lab5(3).cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    int x1, x2, y1, y2, l, h, P, S;
+    cout << "Введите координаты первой вершины прямоугольника: ";
+    cin >> x1 >> y1;
+    cout << "Введите координаты второй вершины прямоугольника: ";
+    cin >> x2 >> y2;
+    l = x2 - x1;
+    h = y1 - y2;
+    P = (l + h) * 2;
+    cout << "Периметр прямоугольника = " << P << endl;
+    S = l * h;
+    cout << "Площадь прямоугольника = " << S;
+    return 0;
+}
