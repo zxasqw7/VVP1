748a0d039b12f45b92a0be501fbc70d51c806128
zxasqw7 <90841897+zxasqw7@users.noreply.github.com>
2021-10-14 12:43:52 +0300

    Update lab5(4).cpp

--- lab5(4)/lab5(4).cpp
+++ lab5(4)/lab5(4).cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main()
 {
     setlocale(LC_ALL, "Ru");
-    float x1, x2, x3, y1, y2, y3, dis1, dis2, dis3, P;
+    float x1, x2, x3, y1, y2, y3, dis1, dis2, dis3, P, p, h, S;
     cout << "Введите координаты первой вершины треугольника: ";
     cin >> x1 >> y1;
     cout << "Введите координаты второй вершины треугольника: ";
@@ -16,6 +16,10 @@ int main()
     dis3 = sqrt(pow((x1 - x3), 2) + pow((y1 - y3), 2));
     P = dis1 + dis2 + dis3;
     cout << "Периметр треугольника = " << P << endl;
+    p = P / 2;
+    h = 2*(sqrt(p*(p-dis1)*(p-dis2)*(p-dis3)))/dis1;
+    S = dis1*h/2;
+    cout << "Площадь треугольника = " << S;
     return 0;
 }
 
