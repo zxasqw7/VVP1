de1a7e9e619736b00401807224e1d89c2ead619d
zxasqw7 <90841897+zxasqw7@users.noreply.github.com>
2021-10-14 15:43:44 +0300

    Update lab5(1).cpp

--- lab5(1)/lab5(1).cpp
+++ lab5(1)/lab5(1).cpp
@@ -1,23 +1,26 @@
-﻿#include <iostream>
+#include <iostream>
 using namespace std;
 
 int main()
 {
     setlocale(LC_ALL, "Ru");
-    int A, B, C, d1, d2, summ;
+    int A, C, B, d1, d2, mult;
     cout << "Введите точку A: ";
     cin >> A;
+    cout << "Введите точку C (должна лежать между точками A и B: ";
+    cin >> C;
     cout << "Введите точку B: ";
     cin >> B;
-    cout << "Введите точку C: ";
-    cin >> C;
+    if (C > B)
+    {
+    cout << "Точка C должна лежать между точками A и B";
+    }
+    else
+    {
     d1 = C - A;
-    cout << "Длина отрезка AC = " << d1 << endl;
-    d2 = C - B;
-    cout << "Длина отрезка BC  = " << d2 << endl;
-    summ = d1 + d2;
-    cout << "Сумма отрезков AC и BC = " << summ;
+    d2 = B - C;
+    mult = d1 * d2;
+    cout << "Произведение длин отрезков = " << mult;
+    }
     return 0;
 }
-
-
