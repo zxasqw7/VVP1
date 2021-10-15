b6e28e85900c60a620253f08d918bda351f75cc7
zxasqw7 <90841897+zxasqw7@users.noreply.github.com>
2021-10-15 15:41:32 +0300

    Update lab5(2).cpp

--- lab5(2)/lab5(2).cpp
+++ lab5(2)/lab5(2).cpp
@@ -1,4 +1,4 @@
-﻿#include <iostream>
+#include <iostream>
 using namespace std;
 
 int main()
@@ -7,14 +7,20 @@ int main()
     int A, C, B, d1, d2, mult;
     cout << "Введите точку A: ";
     cin >> A;
-    cout << "Введите точку C: ";
+    cout << "Введите точку C (должна лежать между точками A и B: ";
     cin >> C;
     cout << "Введите точку B: ";
     cin >> B;
+    if (C > B)
+    {
+    cout << "Точка C должна лежать между точками A и B";
+    }
+    else
+    {
     d1 = C - A;
     d2 = B - C;
     mult = d1 * d2;
     cout << "Произведение длин отрезков = " << mult;
+    }
     return 0;
 }
-
