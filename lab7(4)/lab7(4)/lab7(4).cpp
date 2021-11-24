79998f3fe50bc61d6d4a37c8e525ecb95486884c
zxasqw7 <sviridov-127@yandex.ru>
2021-11-24 17:34:45 +0300

    lab7(4)

---
+++ lab7(4)/lab7(4)/lab7(4).cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    float A, B, x;
+    cout << " Введите число A (не должно быть равно нулю): ";
+    cin >> A;
+    if (A == 0)
+    {
+        cout << "Число 'A' НЕ должно быть равно нулю";
+    }
+    else
+    {
+        cout << " Введите число B: ";
+        cin >> B;
+        x = -B / A;
+        cout << "x = " << x;
+    }
+    return 0;
+}
