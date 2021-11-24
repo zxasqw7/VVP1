84e66216869039c7b60db1f810a2efd22147f963
zxasqw7 <sviridov-127@yandex.ru>
2021-11-24 17:31:50 +0300

    lab6(5)

---
+++ lab6(5)/lab6(5)/lab6(5).cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Ru");
+    int x, y;
+    cout << "Введите число, которое нужно возвести в 8 степень: ";
+    cin >> y;
+    x = y * y;//2 степень
+    x = x * x;//4 степень 
+    x = x * x;// 8 степень
+    cout << "число " << y << " в 8 степени = " << x;
+    return 0;
+}
