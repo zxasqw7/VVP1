6da5d5a9b63c796f66be3d2d5309f17287cb0613
zxasqw7 <sviridov-127@yandex.ru>
2021-11-24 17:32:13 +0300

    lab6(6)

---
+++ lab6(6)/lab6(6)/lab6(6).cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Ru");
+    int x, y, y1;
+    cout << "Введите число, которое нужно возвести в 15 степень: ";
+    cin >> x;
+    y = x * x;//2 степень
+    y1 = y * x;//3 степень
+    y = y1 * y;//5 степень 
+    y1 = y * y;//10 степень 
+    y = y * y1;//15 степень 
+    cout << "число " << x << " в 15 степени = " << y;
+    return 0;
+}
