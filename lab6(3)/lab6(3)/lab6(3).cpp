8f5d4999a196874bba20c0f19ba9dd75cebaa387
zxasqw7 <sviridov-127@yandex.ru>
2021-11-24 17:30:51 +0300

    lab6(3)

---
+++ lab6(3)/lab6(3)/lab6(3).cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Ru");
+	float x, y;
+	cout << "Введите x: ";
+	cin >> x;
+	y = 4 * pow((3 * x), 6) - pow((6 * x), 2) - 7;
+	cout << "y = " << y;
+	return 0;
+}
