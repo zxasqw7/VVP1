6b5503fd729d68d877bbb08213bca8cbe346180e
zxasqw7 <sviridov-127@yandex.ru>
2021-11-24 17:29:43 +0300

    lab6(1)

---
+++ lab6(1)/lab6(1)/lab6(1).cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Ru");
+	int A, B, C, buff;
+	cout << "Введите значение A: ";
+	cin >> A;
+	cout << "Введите значение B : ";
+	cin >> B;
+	cout << "Введите значение C : ";
+	cin >> C;
+	buff = A;
+	A = C;
+	C = B;
+	B = buff;
+	cout << "A = " << A << endl;
+	cout << "B = " << B << endl;
+	cout << "C = " << C;
+	return 0;
+}
