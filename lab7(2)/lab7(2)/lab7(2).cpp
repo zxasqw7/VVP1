0fa6e0333a3df37b0c80f27dd0771f540aa897bf
zxasqw7 <sviridov-127@yandex.ru>
2021-11-24 17:33:54 +0300

    lab7(2)

---
+++ lab7(2)/lab7(2)/lab7(2).cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    float x, a, y, z, v;
+    cout << "Введите вес конфет (кг): ";
+    cin >> x;
+    cout << "Введите цену " << x << " кг конфет: ";
+    cin >> a;
+    z = a / x;
+    cout << "Один кг конфет стоит " << z << " рублей" << endl;
+    cout << "Введите сколько кг конфет вам нужно: ";
+    cin >> y;
+    v = z * y;
+    cout << y << " кг конфет стоит: " << v << " рублей";
+    return 0;
+}
