867d433f07debc7b5fa9390d8be91e59d0852316
zxasqw7 <sviridov-127@yandex.ru>
2021-11-24 17:32:36 +0300

    lab7

---
+++ lab7/lab7/lab7.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    float a, rad, pi;
+    pi = 3.14;
+    cout << "Введите угол (в градусах): ";
+    cin >> a;
+    if (a >= 0 && a <= 360)
+    {
+        rad = a * pi / 180;
+        cout << a << " градусов  = " << rad << " радиан";
+    }
+    else
+        cout << "Введите значение, которое лежит в промежутке от 0 до 360 ";
+    return 0;
+}
+
+    
