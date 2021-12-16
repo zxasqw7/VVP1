9587388ff4c573a8725d19889f50371af4222540
zxasqw7 <sviridov-127@yandex.ru>
2021-12-16 19:31:48 +0300

    lab10(2)

---
+++ lab10(2)/lab10(2)/lab10(2).cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "Rus");
+	int A;
+	cout << "Введите целое положительно число A:";
+	cin >> A;
+	if (A>9 && A<100 && A % 2 == 0) {
+		cout << "Число " << A << " двузначное и четное";
+	}
+	else
+	{
+		cout << "Высказывание, что число A двузначное или четное - ложь";
+	}
+	return 0;
+}
