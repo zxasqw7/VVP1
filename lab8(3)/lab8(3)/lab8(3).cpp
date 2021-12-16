b8a3d6bcf597fb0d008f5c639cdac0dbba03855c
zxasqw7 <sviridov-127@yandex.ru>
2021-12-16 19:27:42 +0300

    lab8(3)

---
+++ lab8(3)/lab8(3)/lab8(3).cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+	int a,b;
+	cout << "Введите двузначное число: ";
+	cin >> a;
+	b = a / 10 + a % 10 * 10;
+	cout << "Число при перестановке цифр: " << b ;
+	return 0;
+}
