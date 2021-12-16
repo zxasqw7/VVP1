0e93d23a28c09b75504f79e1b4498229fb7a05d4
zxasqw7 <sviridov-127@yandex.ru>
2021-12-16 19:31:25 +0300

    lab10(1)

---
+++ lab10(1)/lab10(1)/lab10(1).cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "ru");
+	int A, B, C;
+	cout << "Введите целое число A: ";
+	cin >> A;
+	cout << "Введите целое число B: ";
+	cin >> B;
+	cout << "Введите целое число C: ";
+	cin >> C;
+	if (A < B && B < C)
+		cout << "Истина";
+	else
+		cout << "Ложь";
+	return 0;
+}
+
+
