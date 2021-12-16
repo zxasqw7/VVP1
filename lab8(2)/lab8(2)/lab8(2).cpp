7af02b35e8eeacfee48f79d1a60537b1fcad49e2
zxasqw7 <sviridov-127@yandex.ru>
2021-12-16 19:26:30 +0300

    lab8(2)

---
+++ lab8(2)/lab8(2)/lab8(2).cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <math.h>
+using namespace std;
+int main() {
+    setlocale(LC_ALL, "Russian");
+    int A, B, c, c1;
+    cout << "Введите длину отрезка A: ";
+    cin >> A;
+    cout << "Введите длину отрезка B(B<A): ";
+    cin >> B;
+    if (B > A) {
+        cout << "Отрезок B должен быть меньше чем отрезок A";
+    }
+    else {
+        c = A / B;
+        c1 = A - B * c;
+        cout << "Длина незанятой части = " << c1;
+    }
+    return 0;
+}
+
