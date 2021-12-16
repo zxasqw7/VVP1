8fd2b02a6a203bc2a3b8d1829a156f178b12a41f
zxasqw7 <sviridov-127@yandex.ru>
2021-12-16 19:28:10 +0300

    lab8(4)

---
+++ lab8(4)/lab8(4)/lab8(4).cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+
+	int a, b;
+	cout << "Введите трехзначное число: ";
+	cin >> a;
+    b = (a % 100 / 10 * 100) + (a % 10) * 10 + a / 100;
+	cout << "Полученное число: " << b;
+	return 0;
+}
