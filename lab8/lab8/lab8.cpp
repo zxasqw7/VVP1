47e58cc97735df660a80e0d066b8d2b9f061a476
zxasqw7 <sviridov-127@yandex.ru>
2021-12-16 19:25:19 +0300

    lab8

---
+++ lab8/lab8/lab8.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include <math.h>
+using namespace std;
+int main() {
+    setlocale(LC_ALL, "Russian");
+    int a, b;
+    cout << "Введите сколько байт занимает файл: ";
+    cin >> a;
+    b = a / 1024;
+    cout << "Файл занимает " << b << " Кб ";
+    return 0;
+}
+
+    
