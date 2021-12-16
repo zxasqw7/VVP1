d192b89c0974bd243f9c2c7a99a8a9db7dc3c223
zxasqw7 <sviridov-127@yandex.ru>
2021-12-16 19:25:48 +0300

    lab8(1)

---
+++ lab8(1)/lab8(1)/lab8(1).cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    int A, B, c;
+    cout << "Введите длину отрезка A: ";
+    cin >> A;
+    cout << "Введите длину отрезка B (B<A): ";
+    cin >> B;
+    if (A < B) {
+        cout << "отрезок A должен быть больше чем отрезок B";
+    }
+    else {
+        c = A / B;
+        cout << "На отрезке A" << " размещенно " << c << " отр. B";
+    }
+    return 0;
+}
+    
