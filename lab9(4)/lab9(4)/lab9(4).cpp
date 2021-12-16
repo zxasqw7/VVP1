35c66f74df92775322c3a5833fbfc2bb9f84ddd7
zxasqw7 <sviridov-127@yandex.ru>
2021-12-16 19:30:22 +0300

    lab9(4)

---
+++ lab9(4)/lab9(4)/lab9(4).cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+
+	int y, x;
+	cout << "Введите год: ";
+	cin >> y;
+	x = (y / 100) + 1;
+	cout << y << " год это " << x << " столетие";
+	return 0;
+}
