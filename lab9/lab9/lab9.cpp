9934cae73e0b3449b60828fb1b1e09a7736d359a
zxasqw7 <sviridov-127@yandex.ru>
2021-12-16 19:28:35 +0300

    lab9

---
+++ lab9/lab9/lab9.cpp
@@ -0,0 +1,16 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+
+	int N, m;
+	cout << "Секунды:";
+	cin >> N;
+	cout << endl;
+
+	m = N % 60;
+	cout << "прошло секунд с последней минуты: " << m;
+	return 0;
+}
