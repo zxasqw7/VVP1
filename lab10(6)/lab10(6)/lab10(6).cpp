d828ac8eb419251ea1450a5f11cfe525e67097c7
zxasqw7 <sviridov-127@yandex.ru>
2021-12-16 19:33:19 +0300

    lab10(6)

---
+++ lab10(6)/lab10(6)/lab10(6).cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+	float a, b, c;
+	cout << "Введите первую сторону треугольника: ";
+	cin >> a;
+	cout << "Введите второую сторону треугольника: ";
+	cin >> b;
+	cout << "Введите третью сторону треугольника: ";
+	cin >> c;
+	if (a + b > c)
+		cout << "Треугольник есть" << endl;
+	else if (a + c > b)
+		cout << "Треугольник есть" << endl;
+	else if (b + c > a)
+		cout << "Треугольник есть" << endl;
+	else
+		cout << "Треугольник не является прямоугольным" << endl;
+
+	return 0;
+}
