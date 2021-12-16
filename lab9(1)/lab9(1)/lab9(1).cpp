21a88c9e3eb2f599294e3b13830311570eff08e2
zxasqw7 <sviridov-127@yandex.ru>
2021-12-16 19:29:01 +0300

    lab9(1)

---
+++ lab9(1)/lab9(1)/lab9(1).cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	setlocale(LC_ALL, "rus");
+	int K, x;
+	cout << "Введите день недели (от 1 до 365): ";
+	cin >> K;
+	if (K < 1 || K > 365) {
+		cout << "Число должно лежать в диапазоне от 1 до 365";
+	}
+	else {
+    x = K % 7;
+	cout << "Номер дня недели = " << x;
+	}
+	return 0;
+}
