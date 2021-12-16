413600c134ba1b3d079ff51f4fb20ffd12cfb9e3
zxasqw7 <sviridov-127@yandex.ru>
2021-12-16 19:31:03 +0300

    lab10

---
+++ lab10/lab10/lab10.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "ru");
+	int A, B;
+	cout << "Введите целое чилсло A:";
+	cin >> A;
+	cout << "Введите целое число B:";
+	cin >> B;
+	if (A > 2 && B <= 3)
+		cout << "Число A > 2 и число B <= 3";
+	else
+		cout << "Высказывание A > 2 и B <=3 не верно";
+	return 0;
+}
