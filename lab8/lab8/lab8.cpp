37c5af38fbfc65a3dfbe34e3ce576eb89cbbf3c5
zxasqw7 <90841897+zxasqw7@users.noreply.github.com>
2021-12-26 20:32:07 +0300

    Update lab8.cpp

--- lab8/lab8/lab8.cpp
+++ lab8/lab8/lab8.cpp
@@ -1,13 +1,19 @@
 ﻿#include <iostream>
 #include <math.h>
 using namespace std;
-int main() {
+int main() 
+{
     setlocale(LC_ALL, "Russian");
     int a, b;
     cout << "Введите сколько байт занимает файл: ";
     cin >> a;
     b = a / 1024;
-    cout << "Файл занимает " << b << " Кб ";
+    if (a % 1024 == 0)
+    {
+        cout << "Файл занимает " << b << " Кб ";
+    }
+    else 
+        cout << "Файл занимает " << b + 1 << " Кб ";
     return 0;
 }
 
