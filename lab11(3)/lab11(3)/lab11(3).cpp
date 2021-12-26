c30e031d04cb81b5c9845148d627af054e0a9f7a
zxasqw7 <sviridov-127@yandex.ru>
2021-12-26 20:40:00 +0300

    lab11(3)

---
+++ lab11(3)/lab11(3)/lab11(3).cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() 
+{
+    setlocale(LC_ALL, "Russian");
+    int x, y;
+    cout << "Введите координату по x: ";
+    cin >> x;
+    cout << "Введите координату по y: ";
+    cin >> y;
+
+    if (x > 0 && y > 0) 
+    {
+        cout << "Точка находится в 1-ой четверти";
+    }
+    else if (x < 0 && y>0) 
+    {
+        cout << "Точка находится во 2-ой четверти";
+    }
+    else if (x < 0 && y < 0) 
+    {
+        cout << "Точка находится в 3-ей четверти";
+    }
+    else if (x > 0 && y < 0) 
+    {
+        cout << "Точка находится в 4-ой четверти";
+    }
+    return 0;
+}
