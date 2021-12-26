ebf8752eb950bd29387e27b2d1d002292046a384
zxasqw7 <sviridov-127@yandex.ru>
2021-12-26 20:41:06 +0300

    lab11(4)

---
+++ lab11(4)/lab11(4)/lab11(4).cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+	int a;
+	cout << "Введите число a: ";
+	cin >> a;
+	if (a > 0 && a % 2 == 0)
+	{
+		cout << "Число " << a << " положительное и четное";
+	}
+	else if (a > 0 && a % 2 != 0) 
+	{
+		cout << "Число " << a << " положительное и нечетное";
+	}
+	else if (a < 0 && a % 2 == 0)
+	{
+		cout << "Число " << a << " отрицательное и четное";
+	}
+	else if (a < 0 && a % 2 != 0)
+	{
+		cout << "Число " << a << " отрицательное и нечетное";
+	}
+	return 0;
+}
