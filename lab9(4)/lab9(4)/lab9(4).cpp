00fed030c9b58566ea9d7291f17a9d82656673f5
zxasqw7 <90841897+zxasqw7@users.noreply.github.com>
2021-12-26 20:36:22 +0300

    Update lab9(4).cpp

--- lab9(4)/lab9(4)/lab9(4).cpp
+++ lab9(4)/lab9(4)/lab9(4).cpp
@@ -1,13 +1,18 @@
 ﻿#include <iostream>
 using namespace std;
 
-int main() {
+int main() 
+{
 	setlocale(LC_ALL, "Russian");
-
 	int y, x;
 	cout << "Введите год: ";
 	cin >> y;
-	x = (y / 100) + 1;
-	cout << y << " год это " << x << " столетие";
+	x = (y / 100);
+	if (y % 100 == 0)
+	{
+		cout << y << " год это " << x << " столетие";
+	}
+	else 
+		cout << y << " год это " << x + 1 << " столетие";
 	return 0;
 }
