dae85151607ceaafd96e48898bf0f7a1ae5326e9
zxasqw7 <sviridov-127@yandex.ru>
2021-12-26 19:29:12 +0300

    lab12

---
+++ lab12/lab12/lab12.cpp
@@ -0,0 +1,69 @@
+﻿# include <iostream>
+# include <windows.h>
+# include <cmath>
+# include <iomanip>
+# include <fstream>
+# include <string.h>
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+	int a, b;
+	string c;
+	cout << "Введите день (от 1 до 31): ";
+	cin >> a;
+	cout << "Введите месяц (от 1 до 12): ";
+	cin >> b;
+	switch (a) 
+	{
+	case 1: cout << "Первое "; break;
+	case 2: cout << "Второе "; break;
+	case 3: cout << "Третье "; break;
+	case 4: cout << "Четвертое "; break;
+	case 5: cout << "Пятое "; break;
+	case 6: cout << "Шестое "; break;
+	case 7: cout << "Седьмое "; break;
+	case 8: cout << "Восьмое "; break;
+	case 9: cout << "Девятое "; break;
+	case 10: cout << "Десятое "; break;
+	case 11: cout << "Одиннадцатое "; break;
+	case 12: cout << "Двенядцатое  "; break;
+	case 13: cout << "Тринадцатое "; break;
+	case 14: cout << "Четырнадцатое "; break;
+	case 15: cout << "Пятнадцатое "; break;
+	case 16: cout << "Шестнадцатое "; break;
+	case 17: cout << "Семнадцатое "; break;
+	case 18: cout << "Восемнадцатое "; break;
+	case 19: cout << "Девятнадцатое "; break;
+	case 20: cout << "Двадцатое "; break;
+	case 21: cout << "Двадцать первое "; break;
+	case 22: cout << "Двадцать второе "; break;
+	case 23: cout << "Двадцать третье "; break;
+	case 24: cout << "Двадцать четвертое "; break;
+	case 25: cout << "Двадцать пятое "; break;
+	case 26: cout << "Двадцать шестое "; break;
+	case 27: cout << "Двадцать седьмое "; break;
+	case 28: cout << "Двадцать восьмое "; break;
+	case 29: cout << "Двадцать девятое "; break;
+	case 30: cout << "Тридцатое "; break;
+	case 31: cout << "Тридцать первое "; break;
+	}
+	switch (b) 
+	{
+	case 1: cout << "января"; break;
+	case 2: cout << "февраля"; break;
+	case 3: cout << "марта"; break;
+	case 4: cout << "апреля"; break;
+	case 5: cout << "мая"; break;
+	case 6: cout << "июня"; break;
+	case 7: cout << "июля"; break;
+	case 8: cout << "августа"; break;
+	case 9: cout << "сентября"; break;
+	case 10: cout << "октября"; break;
+	case 11: cout << "ноября"; break;
+	case 12: cout << "декабря"; break;
+	}
+	cout << endl;
+	system("pause");
+	return 0;
+}
