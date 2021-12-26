ba630640b270c05978f5bfe4aca6bf73a9a1084c
zxasqw7 <sviridov-127@yandex.ru>
2021-12-26 20:37:57 +0300

    lab11

---
+++ lab11/lab11/lab11.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+int main() 
+{
+	setlocale(LC_ALL, "ru");
+	int A, B;
+	cout << " Введите целое число A: ";
+	cin >> A;
+	cout << "Введите целое число B: ";
+	cin >> B;
+	if (A > B) 
+	{
+		B = A;
+		cout << "A = " << A << endl << "B = " << B;
+	}
+	else if (B > A) 
+	{
+		A = B;
+		cout << "A = " << A << endl << "B = " << B;
+	}
+	else 
+	{
+		A = 0;
+		B = 0;
+		cout << "A = " << A << endl << "B = " << B;
+	}
+	return 0;
+}
