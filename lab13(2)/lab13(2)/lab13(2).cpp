455cd9d0c7e5c77b39dc4467af91cfeb061652a3
zxasqw7 <sviridov-127@yandex.ru>
2021-12-30 15:32:00 +0300

    lab13(2)

---
+++ lab13(2)/lab13(2)/lab13(2).cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    int N, i, a;
+    a = 0;
+    cout << "Введите целое число N (N > 0): ";
+    cin >> N;
+    i = 1;
+    while (i <= 2 * N - 1) 
+    {
+        a = a + i;
+        cout << a << endl;
+        i = i + 2;
+    }
+    return 0;
+}
