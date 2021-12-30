706a1d7e73da48d1118b377700440663669d4701
zxasqw7 <sviridov-127@yandex.ru>
2021-12-30 15:33:50 +0300

    lab14(2)

---
+++ lab14(2)/lab14(2)/lab14(2).cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, k, s;
+    cout << " Введите N (N > 1): ";
+    cin >> n;
+    k = 0;
+    s = 0;
+    while (s <= n)
+    {
+        k += 1;
+        s += k;
+    }
+    cout << "k:" << k << "\ns:" << s;
+    return 0;
+}
