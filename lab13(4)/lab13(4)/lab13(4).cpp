6f6c2f2c65b91d2257a51811f6a0e60f8f832026
zxasqw7 <sviridov-127@yandex.ru>
2021-12-30 15:32:49 +0300

    lab13(4)

---
+++ lab13(4)/lab13(4)/lab13(4).cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    float A, h, res;
+    cout << "Введите вещественное число A: ";
+    cin >> A;
+    int N;
+    cout << "Введите целое число N (N > 0): ";
+    cin >> N;
+    h = 1;
+    res = 1;
+    int i;
+    for (i = 1; i <= N; ++i)
+    {
+        h *= -A;
+        res += h;
+    }
+    cout << "Значение выражения  = " << res;
+}
