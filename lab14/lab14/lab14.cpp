d0716913c6a2c9d79b07e6100821a81eeb9662bc
zxasqw7 <sviridov-127@yandex.ru>
2021-12-30 15:33:05 +0300

    lab14

---
+++ lab14/lab14/lab14.cpp
@@ -0,0 +1,23 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    int A, B;
+    cout << "Введите число A: ";
+    cin >> A;
+    cout << "Введите число B (B > A): ";
+    cin >> B;
+
+    int i, j;
+    for (i = A; i <= B; ++i) 
+    {
+        for (j = 1; j <= i; ++j) 
+        {
+            cout << i << " ";
+        }
+            
+    }
+    return 0;
+}
