064b94f2b09c2495f0f6cb4840e75f0ceb9c4f1d
zxasqw7 <sviridov-127@yandex.ru>
2021-12-30 15:35:33 +0300

    lab14(4)

---
+++ lab14(4)/lab14(4)/lab14(4).cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    int A, B;
+    cout << "Введите целое положительное число A: ";
+    cin >> A;
+    cout << "Введите целое положительное число B:";
+    cin >> B;
+    cout << "НОД чисел " << A << " и " << B << " = ";
+    while ((A != 0) & (B != 0)) 
+    {
+        if (A > B) 
+        {
+            A = A % B;
+        }
+        else 
+        {
+            B = B % A;
+        }
+    }
+    cout << A + B;
+    return 0;
+}
