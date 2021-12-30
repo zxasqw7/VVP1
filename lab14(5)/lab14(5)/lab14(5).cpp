23707ae1f45c10e5059173219f96563f06ad6230
zxasqw7 <sviridov-127@yandex.ru>
2021-12-30 15:36:20 +0300

    lab14(5)

---
+++ lab14(5)/lab14(5)/lab14(5).cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    int N;
+    cout << "N:";
+    cin >> N;
+    int f1 = 1, f2 = 1, f = 0, k = 2;
+    while (f < N) {
+        ++k;
+        f = f2 + f1;
+        f2 = f1;
+        f1 = f;
+    }
+    cout << "k:" << k;
+    return 0;
+}
