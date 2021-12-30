d48a5c5834af4fcb620a76a45140349ca5f8cb2f
zxasqw7 <sviridov-127@yandex.ru>
2021-12-30 15:32:23 +0300

    lab13(3)

---
+++ lab13(3)/lab13(3)/lab13(3).cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    float A, h, res;
+    h = 1;
+    res = 1;
+    cout << "Введите вещественное число A: ";
+    cin >> A;
+    int N, i;
+    cout << "Введите целое число N (N > 0): ";
+    i = 1;
+    cin >> N;
+    while (i <= N)
+    {
+        h = h * A;
+        res = res + h;
+        ++i;
+    }
+    cout << "Сумма = " << res;
+}
