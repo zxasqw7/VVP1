6408fde5af67c62ae0dfe2e5edeb071230600d13
zxasqw7 <sviridov-127@yandex.ru>
2021-12-30 15:31:30 +0300

    lab13(1)

---
+++ lab13(1)/lab13(1)/lab13(1).cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "ru");
+    int N, i;
+    double h, a;
+    h = 1;
+    a = 1;
+    cout << "Введите число N ( N > 0): ";
+    cin >> N;
+    i = 0;
+    while (i <= N)
+    {
+        h = h * a;
+        a = a + 0.1;
+        i++;
+    }
+    cout << "Произведение сомножителей = " << h;
+    return 0;
+}
