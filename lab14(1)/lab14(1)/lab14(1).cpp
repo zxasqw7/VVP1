75545da92abad75111f4b090a9e3d162ec7475e7
zxasqw7 <sviridov-127@yandex.ru>
2021-12-30 15:33:25 +0300

    lab14(1)

---
+++ lab14(1)/lab14(1)/lab14(1).cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "Rus");
+	int a, b, buff;
+	cout << "Введите отрезок A: ";
+	cin >> a;
+	cout << "Введите отрезок B (B<A): ";
+	cin >> b;
+
+	buff = a;
+	while (buff >= b) 
+	{
+		buff -= b;
+	}
+	cout << buff ;
+}
