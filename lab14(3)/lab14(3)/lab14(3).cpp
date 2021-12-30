2888a82ca01e160e98d50adf35b6d3104a79b399
zxasqw7 <sviridov-127@yandex.ru>
2021-12-30 15:34:21 +0300

    lab14(3)

---
+++ lab14(3)/lab14(3)/lab14(3).cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    float p;
+    cout << "Введите процент P (от 0 до 25): ";
+    cin >> p;
+    int k = 0;
+    float s = 1000;
+    while (s <= 1100) 
+    {
+        k++;
+        s += s * p / 100;
+    }
+    cout << "кол-во месяцев: " << k << endl << "Итоговый размер вклада: " << s;
+    return 0;
+}
