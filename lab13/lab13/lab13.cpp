785ef9f2c1053b22a91261845349ec5809359d94
zxasqw7 <sviridov-127@yandex.ru>
2021-12-30 15:31:11 +0300

    lab13

---
+++ lab13/lab13/lab13.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+	float a, b, res;
+	cout << "Введите стоимость конфет за 1 килограмм: ";
+	cin >> a;
+	b = 0.1;
+	while (b < 1.1)
+	{
+		res = a * b;
+		cout << "Ценна за " << b << " килограмм конфет = " << res << endl;
+		b = b + 0.1;
+	}
+	return 0;
+}
