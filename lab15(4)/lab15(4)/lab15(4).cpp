dfe2492a32b8077ae67623cbc345aef9c0e891c6
zxasqw7 <sviridov-127@yandex.ru>
2022-01-20 16:06:45 +0300

    lab15(4)

---
+++ lab15(4)/lab15(4)/lab15(4).cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+
+float fact2(int n) {
+	int buff = 1;
+	for (; n > 0; n = n - 2) {
+		buff = buff * n;
+	}
+	return buff;
+}
+int main(void) {
+	setlocale(LC_ALL, "ru");
+	int i, n;
+	cout << "Введите число n: ";
+	cin >> n;
+	fact2(n);
+	cout << fact2(n);
+	return 0;
+}
