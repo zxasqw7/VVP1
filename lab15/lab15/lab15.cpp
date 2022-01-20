0044d9940e5a4c55c55f159002e79304f76c8365
zxasqw7 <sviridov-127@yandex.ru>
2022-01-20 16:05:01 +0300

    lab15

---
+++ lab15/lab15/lab15.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+int powera3(int a) {
+	int b = a * a * a;
+	cout << "B = " << b << endl;
+	return b;
+	
+}
+int main() {
+	setlocale(LC_ALL, "ru");
+	int a, i;
+	i = 0;
+	while (i < 5) {
+		cout << "Введите число a: ";
+		cin >> a;
+		powera3(a);
+		i++;
+	}
+	return 0;
+}
