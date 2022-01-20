697209249c8953c90d075cc5b76f6145cb492cf4
zxasqw7 <sviridov-127@yandex.ru>
2022-01-20 16:06:18 +0300

    lab15(3)

---
+++ lab15(3)/lab15(3)/lab15(3).cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+using namespace std;
+
+int quarter(int x, int y) {
+	if (x > 0 && y > 0) {
+		return 1;
+	}
+	else if (x < 0 && y > 0) {
+		return 2;
+	}
+	else if (x < 0 && y < 0) {
+		return 3;
+	}
+	else if (x > 0 && y < 0) {
+		return 4;
+	}
+}
+int main() {
+	setlocale(LC_ALL, "ru");
+	for (int i = 0; i < 3; i++) 
+	{
+		int x, y;
+		cout << "Введите координату по x: ";
+		cin >> x;
+		cout << "Введите координату по y: ";
+		cin >> y;
+		quarter(x, y);
+		cout << quarter(x, y) << endl;
+	}
+	return 0;
+}
