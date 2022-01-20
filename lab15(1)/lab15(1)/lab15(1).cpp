63e9a4651d7387a2f0025e6ddb5587b5695e32ad
zxasqw7 <sviridov-127@yandex.ru>
2022-01-20 16:05:25 +0300

    lab15(1)

---
+++ lab15(1)/lab15(1)/lab15(1).cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+
+int sign(int x) {
+	if (x < 0) {
+		return -1;
+	}
+	else if (x == 0) {
+		return 0;
+	}
+	else if (x > 0) {
+		return 1;
+	}
+	return x;
+}
+int main() {
+	setlocale(LC_ALL, "ru");
+	int a, b;
+	cout << "Введите число a: ";
+	cin >> a;
+	cout << "Введите число b: ";
+	cin >> b;
+	cout << sign(a) + sign(b);
+	return 0;
+}
